9604ee395f6f57666b3b6cf33cced0ec1395db39
KwangJeCho <dreamer@sangsangsoft.co.kr>
2016-03-13 17:25:15 +0900

    Create Project

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by ckj6911-mac on 2016. 3. 13..
+//  Copyright © 2016년 ckj6911-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //test 합니다
+    // 변경1
+    
+
+}
+
